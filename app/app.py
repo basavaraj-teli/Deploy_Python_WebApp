ec2cedf0aa67d9e4487998c74a906871b631d475
Basavaraj Teli <90122599+basavaraj-teli@users.noreply.github.com>
2026-04-05 14:08:26 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello!!! Basavaraj here!"
+    return <h1>"Hello!!! Basavaraj here!"</h1>
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
