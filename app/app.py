c73e765af0ae956867f3da0f9c02ab315cd171f5
Basavaraj Teli <90122599+basavaraj-teli@users.noreply.github.com>
2026-04-05 14:14:25 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return <h1>"Hello!!! Basavaraj here!"</h1>
+    return '<h1>"Hello, This my DevOps Project"</h1> <br> <p>"Demonstrates end-to-end production-style deployment on AWS using Terraform, Docker, ECR, and GitHub Actions"</p>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
